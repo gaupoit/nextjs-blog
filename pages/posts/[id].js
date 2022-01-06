e65b244cf40d89ce22d446fd044c848f33ecdc20
Thinh Nguyen <thinhnp@ymese.com>
2022-01-06 19:35:23 +0700

    Fix isFallback

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,22 +3,30 @@ import Date from "../../components/date";
 import Layout from "../../components/layout";
 import Parse from "../../services/parse";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Post({ postData }) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) return <h1>Loading</h1>;
   return (
     <Layout>
       <Head>
-        <title>{postData.title}</title>
+        <title>Deeplink</title>
       </Head>
       <article>
-        <h1 className="text-3xl font-bold underline">VieFAM Post</h1>
-        <Image src={postData.title} width={300} height={300} />
+        <h1 className="text-3xl font-bold underline">
+          Testing Deeplink's Post
+        </h1>
+        {postData.title && (
+          <Image src={postData.title} width={300} height={300} />
+        )}
         {/* <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
         <div>
-          <a href="">Open in app</a>
+          <a href="https://links.viefam.com/D2mSAd4HRiLB85gF7">Open in app</a>
         </div>
       </article>
     </Layout>
@@ -42,13 +50,23 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const query = new Parse.Query("Post");
-  const post = await query.get(params.id);
-  const postData = {
-    title: post.get("images")[0],
-  };
-  return {
-    props: {
-      postData,
-    },
-  };
+  try {
+    const post = await query.get(params.id);
+    if (!post) {
+      return { notFound: true };
+    }
+    const postData = {
+      title: post ? post.get("images")[0] : "Not found",
+    };
+    return {
+      props: {
+        postData,
+      },
+    };
+  } catch (error) {
+    console.log("Post", error);
+    return {
+      notFound: true,
+    };
+  }
 }
